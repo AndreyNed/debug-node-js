f4d31678a096abd6e90c374fb10e7c4404507343
a_nedaseikin <andrei.nedaseikin@gmail.com>
2021-05-21 20:23:53 +0300

    fix: fix imports, body parsers, fix starting of server

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 var express = require('express');
-var app = express();
+const bodyParser = require('body-parser');
+
 var db = require('./db');
 var user = require('./controllers/usercontroller');
+const validateSession = require('./middleware/validate-session');
 var game = require('./controllers/gamecontroller')
 
+const PORT = Number(process.env.PORT);
+var app = express();
 
 db.sync();
-app.use(require('body-parser'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use('/api/auth', user);
-app.use(require('./middleware/validate-session'))
+app.use(validateSession);
 app.use('/api/game', game);
-app.listen(function() {
+
+app.all('/', (req, res) => { res.send('Hello!'); });
+
+app.listen(PORT,function() {
     console.log("App is listening on 4000");
-})
+});
